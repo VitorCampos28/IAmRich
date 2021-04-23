8410b5b226a9b1930035372b21c15220ff99d992
Vitor <Vito.r.natal@hotmail.com>
2021-04-23 17:47:34 -0400

    First Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by user194368 on 4/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
